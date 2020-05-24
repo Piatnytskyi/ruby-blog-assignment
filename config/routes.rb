d1d227bc251826147ed8fc6452defe6dfd1a92e7
Vitalii Piatnytskyi <vitaliipiatnytskyi@yahoo.com>
2020-05-24 13:12:02 +0300

    devise and simple_form gems added, primitive user system implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  root 'posts#index'
+  devise_for :users
+  root 'pages#index'
+  get 'posts', to: 'pages#index'
+  get 'about', to: 'pages#about'
   resources :posts
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'dev@example.com',
+    password: 'devpass',
+    password_confirmation: 'devpass',
+    admin: true)
